da4d1d48b9994f21d8518b672a1d050ff45b206a
abdus.salam@savethechildren.org <abdus.salam@savethechildren.org>
2026-01-29 14:26:26 +0600

    ExtendedUserInfo added for AspNetUsers extra info added

---
+++ Domain/Aggregates/ExtendedUserInfo.cs
@@ -0,0 +1,13 @@
+﻿using RapidFireLib.Lib.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Aggregates
+{
+    public class ExtendedUserInfo : IModel
+    {
+        [Key]
+        public string ExtendedUserInfoId { get; set; }
+        //[Required]
+        public string StaffId { get; set; }
+    }
+}
